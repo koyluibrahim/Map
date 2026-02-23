bb88e15c7b1b78cfc6d9f1882be762bdddeaaae8
İbrahim Köylü <119944742+koyluibrahim@users.noreply.github.com>
2026-02-23 14:27:26 +0300

    Refactor Form1 class and improve GoButton_Click logic

--- jdoodle.cs
+++ jdoodle.cs
@@ -1,40 +1,71 @@
-public partial class Form1 : Form
+using System;
+using System.Globalization;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+
+namespace TurkiyeHarita
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
-        InitializeMap();
+        public Form1()
+        {
+            InitializeComponent();
 
-        goButton.Click += GoButton_Click;
+            InitializeMap();
+
+            // Connect button click event
+            goButton.Click += GoButton_Click;
+        }
+
+        private void InitializeMap()
+        {
+            mapControl.MapProvider = OpenStreetMapProvider.Instance;
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+
+            mapControl.MinZoom = 2;
+            mapControl.MaxZoom = 18;
+            mapControl.Zoom = 6;
+
+            // Initial position: Turkey center
+            mapControl.Position = new PointLatLng(39.0, 35.0);
+        }
+
+        private void GoButton_Click(object sender, EventArgs e)
+        {
+            bool latitudeValid = double.TryParse(
+                latitudeTextBox.Text,
+                NumberStyles.Float,
+                CultureInfo.InvariantCulture,
+                out double latitude);
+
+            bool longitudeValid = double.TryParse(
+                longitudeTextBox.Text,
+                NumberStyles.Float,
+                CultureInfo.InvariantCulture,
+                out double longitude);
+
+            if (!latitudeValid || !longitudeValid)
+            {
+                MessageBox.Show("Please enter valid coordinates using a dot. Example: 41.0151");
+                return;
+            }
+
+            if (latitude < -90 || latitude > 90 || longitude < -180 || longitude > 180)
+            {
+                MessageBox.Show("Coordinates are out of range.");
+                return;
+            }
+
+            mapControl.Position = new PointLatLng(latitude, longitude);
+
+            if (mapControl.Zoom < 12)
+            {
+                mapControl.Zoom = 12;
+            }
+        }
     }
-
-    private void InitializeMap()
-    {
-        mapControl.MapProvider = OpenStreetMapProvider.Instance;
-        GMaps.Instance.Mode = AccessMode.ServerOnly;
-
-        mapControl.MinZoom = 2;
-        mapControl.MaxZoom = 18;
-        mapControl.Zoom = 6;
-
-        // Initial position: center of Turkey
-        mapControl.Position = new PointLatLng(39.0, 35.0);
-    }
-
-    private void GoButton_Click(object sender, EventArgs e)
-    {
-        bool latitudeValid = double.TryParse(
-            latitudeTextBox.Text,
-            NumberStyles.Float,
-            CultureInfo.InvariantCulture,
-            out double latitude);
-
-        bool longitudeValid = double.TryParse(
-            longitudeTextBox.Text,
-            NumberStyles.Float,
-            CultureInfo.InvariantCulture,
-            out double longitude);
-
+}
         if (!latitudeValid || !longitudeValid)
         {
             MessageBox.Show("Please enter valid numeric coordinates using a dot. Example: 41.01");
