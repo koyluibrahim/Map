4b598a149be80d8414014d5180b1178345b70cc1
İbrahim Köylü <119944742+koyluibrahim@users.noreply.github.com>
2026-04-04 19:50:54 +0300

    Create Form1.Designer.cs

---
+++ v2/Form1.Designer.cs
@@ -0,0 +1,21 @@
+namespace TurkiyeMap
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+        }
+    }
+}
