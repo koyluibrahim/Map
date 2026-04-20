a840b9a544aab37b3c33ae495731449a51e12eac
İbrahim Köylü <119944742+koyluibrahim@users.noreply.github.com>
2026-04-20 08:50:31 +0300

    Update design.cs

--- OfflineMap/design.cs
+++ OfflineMap/design.cs
@@ -1,115 +1,126 @@
-partial class Form1
+namespace OfflineMapApp
 {
-    private System.ComponentModel.IContainer components = null;
-
-    private System.Windows.Forms.Panel toolBar;
-    private System.Windows.Forms.Panel panel1;
-    private System.Windows.Forms.Button btnOpen;
-    private System.Windows.Forms.Label lblLat;
-    private System.Windows.Forms.TextBox txtLat;
-    private System.Windows.Forms.Label lblLon;
-    private System.Windows.Forms.TextBox txtLon;
-    private System.Windows.Forms.Label lblZoom;
-    private System.Windows.Forms.TextBox txtZoom;
-    private System.Windows.Forms.Button btnGo;
-    private System.Windows.Forms.Label lblStatus;
-
-    protected override void Dispose(bool disposing)
+    partial class Form1
     {
-        if (disposing && (components != null)) components.Dispose();
-        base.Dispose(disposing);
-    }
+        private System.ComponentModel.IContainer components = null;
 
-    private void InitializeComponent()
-    {
-        toolBar   = new System.Windows.Forms.Panel();
-        panel1    = new System.Windows.Forms.Panel();
-        btnOpen   = new System.Windows.Forms.Button();
-        lblLat    = new System.Windows.Forms.Label();
-        txtLat    = new System.Windows.Forms.TextBox();
-        lblLon    = new System.Windows.Forms.Label();
-        txtLon    = new System.Windows.Forms.TextBox();
-        lblZoom   = new System.Windows.Forms.Label();
-        txtZoom   = new System.Windows.Forms.TextBox();
-        btnGo     = new System.Windows.Forms.Button();
-        lblStatus = new System.Windows.Forms.Label();
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-        // toolBar
-        toolBar.Dock      = System.Windows.Forms.DockStyle.Top;
-        toolBar.Height    = 38;
-        toolBar.BackColor = System.Drawing.Color.FromArgb(50, 50, 50);
+        private void InitializeComponent()
+        {
+            this.txtLat = new System.Windows.Forms.TextBox();
+            this.txtLon = new System.Windows.Forms.TextBox();
+            this.numZoom = new System.Windows.Forms.NumericUpDown();
+            this.btnGit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numZoom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtLat
+            // 
+            this.txtLat.Location = new System.Drawing.Point(60, 12);
+            this.txtLat.Name = "txtLat";
+            this.txtLat.Size = new System.Drawing.Size(100, 20);
+            this.txtLat.TabIndex = 0;
+            this.txtLat.Text = "39.92077"; // Örnek: Ankara Enlem
+            // 
+            // txtLon
+            // 
+            this.txtLon.Location = new System.Drawing.Point(220, 12);
+            this.txtLon.Name = "txtLon";
+            this.txtLon.Size = new System.Drawing.Size(100, 20);
+            this.txtLon.TabIndex = 1;
+            this.txtLon.Text = "32.85411"; // Örnek: Ankara Boylam
+            // 
+            // numZoom
+            // 
+            this.numZoom.Location = new System.Drawing.Point(380, 12);
+            this.numZoom.Maximum = new decimal(new int[] { 20, 0, 0, 0 });
+            this.numZoom.Name = "numZoom";
+            this.numZoom.Size = new System.Drawing.Size(50, 20);
+            this.numZoom.TabIndex = 2;
+            this.numZoom.Value = new decimal(new int[] { 10, 0, 0, 0 });
+            // 
+            // btnGit
+            // 
+            this.btnGit.Location = new System.Drawing.Point(450, 10);
+            this.btnGit.Name = "btnGit";
+            this.btnGit.Size = new System.Drawing.Size(75, 23);
+            this.btnGit.TabIndex = 3;
+            this.btnGit.Text = "Haritayı Bul";
+            this.btnGit.UseVisualStyleBackColor = true;
+            this.btnGit.Click += new System.EventHandler(this.btnGit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512); // İki karoyu yan yana almak istersen büyütebilirsin
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Text = "Enlem:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 15);
+            this.label2.Name = "label2";
+            this.label2.Text = "Boylam:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(335, 15);
+            this.label3.Name = "label3";
+            this.label3.Text = "Zoom:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 580);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnGit);
+            this.Controls.Add(this.numZoom);
+            this.Controls.Add(this.txtLon);
+            this.Controls.Add(this.txtLat);
+            this.Name = "Form1";
+            this.Text = "Çevrimdışı Harita Görüntüleyici";
+            ((System.ComponentModel.ISupportInitialize)(this.numZoom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
 
-        // btnOpen
-        btnOpen.Text     = "MBTiles Aç";
-        btnOpen.Size     = new System.Drawing.Size(100, 26);
-        btnOpen.Location = new System.Drawing.Point(6, 6);
-        btnOpen.Click   += new System.EventHandler(this.btnOpen_Click);
-
-        // lblLat
-        lblLat.Text      = "Lat:";
-        lblLat.ForeColor = System.Drawing.Color.White;
-        lblLat.Size      = new System.Drawing.Size(28, 26);
-        lblLat.Location  = new System.Drawing.Point(116, 9);
-        lblLat.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-
-        // txtLat
-        txtLat.Text     = "39.0";
-        txtLat.Size     = new System.Drawing.Size(75, 22);
-        txtLat.Location = new System.Drawing.Point(146, 8);
-        txtLat.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtCoord_KeyDown);
-
-        // lblLon
-        lblLon.Text      = "Lon:";
-        lblLon.ForeColor = System.Drawing.Color.White;
-        lblLon.Size      = new System.Drawing.Size(28, 26);
-        lblLon.Location  = new System.Drawing.Point(230, 9);
-        lblLon.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-
-        // txtLon
-        txtLon.Text     = "35.0";
-        txtLon.Size     = new System.Drawing.Size(75, 22);
-        txtLon.Location = new System.Drawing.Point(260, 8);
-        txtLon.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtCoord_KeyDown);
-
-        // lblZoom
-        lblZoom.Text      = "Zoom:";
-        lblZoom.ForeColor = System.Drawing.Color.White;
-        lblZoom.Size      = new System.Drawing.Size(38, 26);
-        lblZoom.Location  = new System.Drawing.Point(344, 9);
-        lblZoom.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-
-        // txtZoom
-        txtZoom.Text     = "6";
-        txtZoom.Size     = new System.Drawing.Size(36, 22);
-        txtZoom.Location = new System.Drawing.Point(384, 8);
-        txtZoom.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtCoord_KeyDown);
-
-        // btnGo
-        btnGo.Text     = "Git";
-        btnGo.Size     = new System.Drawing.Size(55, 26);
-        btnGo.Location = new System.Drawing.Point(428, 6);
-        btnGo.Click   += new System.EventHandler(this.btnGo_Click);
-
-        // lblStatus
-        lblStatus.Text      = "Hazır";
-        lblStatus.ForeColor = System.Drawing.Color.LightGray;
-        lblStatus.Size      = new System.Drawing.Size(300, 26);
-        lblStatus.Location  = new System.Drawing.Point(494, 9);
-
-        toolBar.Controls.AddRange(new System.Windows.Forms.Control[] {
-            btnOpen, lblLat, txtLat, lblLon, txtLon,
-            lblZoom, txtZoom, btnGo, lblStatus
-        });
-
-        // panel1
-        panel1.Dock      = System.Windows.Forms.DockStyle.Fill;
-        panel1.BackColor = System.Drawing.Color.Black;
-
-        // Form
-        this.Text          = "MBTiles Viewer";
-        this.Size          = new System.Drawing.Size(900, 650);
-        this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-        this.Controls.Add(panel1);
-        this.Controls.Add(toolBar);
+        private System.Windows.Forms.TextBox txtLat;
+        private System.Windows.Forms.TextBox txtLon;
+        private System.Windows.Forms.NumericUpDown numZoom;
+        private System.Windows.Forms.Button btnGit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
